d5f3324b7d6fcc7a2e6d8eed53a818e25876ea7f
Sergio Chocobar <sergiochocobar2@gmail.com>
2025-10-28 18:45:30 -0300

    Initial Commit

---
+++ MeliChallenge/MeliChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeliChallengeApp.swift
+//  MeliChallenge
+//
+//  Created by NaranjaX on 28/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct MeliChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
